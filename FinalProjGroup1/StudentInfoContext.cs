4040787cb60063c8e6ca82f2244689216eae99e9
ZachtMac <zachary.t.mcintosh@outlook.com>
2021-11-18 13:47:25 -0500

    Did some things, forget what they were. think I added a context who knows.

--- FinalProjGroup1/StudentInfoContext.cs
+++ FinalProjGroup1/StudentInfoContext.cs
@@ -1,11 +1,20 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
+
 namespace FinalProjGroup1
 {
-    public class StudentInfoContext
+    public class StudentInfoContext : DbContext
     {
+        public StudentInfoContext(DbContextOptions<StudentInfoContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<StudentInfoContext> TodoItems { get; set; } = null!;
+
     }
 }
